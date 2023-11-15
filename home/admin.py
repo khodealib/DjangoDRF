9765d790e6bd721c9d0c3ac506b91be391b2c757
Ali Bagheri <khodelaib@gmail.com>
2023-11-15 19:47:02 +0330

    add question and answer models.

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from home.models import Person
+from home.models import Person, Question, Answer
 
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
     pass
--- home/models.py
+++ home/models.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
 
 
 class Person(models.Model):
@@ -8,3 +11,24 @@ class Person(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Question(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='questions')
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.title[:20]}'
+
+
+class Answer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='answers')
+    question = models.ForeignKey('Question', on_delete=models.CASCADE, related_name='answers')
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.question.title[:20]}'
